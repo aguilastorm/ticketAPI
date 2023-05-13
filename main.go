a8df778a083a42e6c9aa4fbaf61e2396da2c592e
Lucas Tobon <lucas.tobon.v@gmail.com>
2023-05-13 11:39:31 -0500

    All routes were created on main

--- main.go
+++ main.go
@@ -11,6 +11,10 @@ func main() {
 	router := mux.NewRouter().StrictSlash(true)
 
 	router.HandleFunc("/tickets", handlers.GetTickets).Methods("GET")
+	router.HandleFunc("/tickets/{id}", handlers.GetTicket).Methods("GET")
+	router.HandleFunc("/tickets", handlers.CreateTicket).Methods("POST")
+	router.HandleFunc("/tickets/{id}", handlers.DeleteTicket).Methods("DELETE")
+	router.HandleFunc("/tickets/{id}", handlers.UpdateTicket).Methods("PUT")
 
 	fmt.Println("Server running on port 8080")
 	http.ListenAndServe(":8080", router)
