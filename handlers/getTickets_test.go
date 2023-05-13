e689cca18db21fd3295cec860dd038e858f74d5e
Lucas Tobon <lucas.tobon.v@gmail.com>
2023-05-13 10:12:30 -0500

    Split test file

--- handlers/getTickets_test.go
+++ handlers/getTickets_test.go
@@ -4,8 +4,6 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"time"
-	"github.com/gorilla/mux"
 	"github.com/aguilastorm/ticketAPI/models"
 )
 
@@ -21,25 +19,6 @@ func TestGetTickets(t *testing.T) {
 	handler := http.HandlerFunc(GetTickets)
 
 	handler.ServeHTTP(rr, req)
-
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
-	}
-}
-
-func TestGetTicket(t *testing.T) {
-	tickets = append(tickets, models.Ticket{ID: "1", User: "Test User", CreationDate: time.Now(), UpdateDate: time.Now(), Status: "open"})
-	req, err := http.NewRequest("GET", "/tickets/1", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rr := httptest.NewRecorder()
-
-	router := mux.NewRouter()
-	router.HandleFunc("/tickets/{id}", GetTicket)
-	router.ServeHTTP(rr, req)
 
 	if status := rr.Code; status != http.StatusOK {
 		t.Errorf("handler returned wrong status code: got %v want %v",
